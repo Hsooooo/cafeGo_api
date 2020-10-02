c17ec6313ce389011fdec53c7c6b89fba11055b0
LeeGwiHyun <chungwhols@ezens.net>
2020-10-02 16:11:27 +0900

    [cafeGo_admin] 로그인 API 생성

--- src/main/java/co/kr/cafego/api/member/MemberController.java
+++ src/main/java/co/kr/cafego/api/member/MemberController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import co.kr.cafego.core.support.ApiSupport;
 
@@ -50,5 +51,34 @@ public class MemberController extends ApiSupport {
 		
 		return obj;
 		
+	}
+
+	/**
+	 * 1.2. 로그인 (ADMIN 로그인)
+	 * @param headers
+	 * @param request
+	 * @return
+	 */
+	
+	public Object login(@RequestHeader HttpHeaders headers, HttpServletRequest request
+			, HttpServletResponse response, Model mode) {
+		
+		Object obj = null;
+		Map<String, String> paramMap = new HashMap<String, String>();
+		
+		String adminId = "";
+		String adminPw = "";
+		// 어드민 아이디
+		adminId = ((String)request.getParameter("adminId"));
+		//어드민 비밀번호
+		adminPw = ((String)request.getParameter("adminPw"));
+		
+		paramMap.put("adminId", adminId);
+		paramMap.put("adminPw", adminPw);
+		
+		
+		obj = memberService.loginSign(paramMap);
+		
+		return obj;
 	}
 }
