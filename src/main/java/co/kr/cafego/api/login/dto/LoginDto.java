b43882b0dff21cb80b81118f417f61ed41b82662
LeeGwiHyun <chungwhols@ezens.net>
2020-10-03 23:16:42 +0900

    -로그인 관련 DTO 추가

--- src/main/java/co/kr/cafego/api/login/dto/LoginDto.java
+++ src/main/java/co/kr/cafego/api/login/dto/LoginDto.java
@@ -1,5 +1,61 @@
 package co.kr.cafego.api.login.dto;
 
-public class LoginDto {
+import java.sql.Date;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class LoginDto {
+	/**
+	 * 관리자 아이디
+	 */
+	private String adminId;
+	/**
+	 * 관리자 패스워드
+	 */
+	private String adminPwd;
+	/**
+	 * 관리 매장명
+	 */
+	private String storeName;
+	/**
+	 * 관리 매장코드
+	 */
+	private String storeCode;
+	/**
+	 * 최근 로그인 접속 시간
+	 */
+	private Date loginDate;
+	
+	/** getters & setters */
+	public String getAdminId() {
+		return adminId;
+	}
+	public void setAdminId(String adminId) {
+		this.adminId = adminId;
+	}
+	public String getAdminPwd() {
+		return adminPwd;
+	}
+	public void setAdminPwd(String adminPwd) {
+		this.adminPwd = adminPwd;
+	}
+	public String getStoreName() {
+		return storeName;
+	}
+	public void setStoreName(String storeName) {
+		this.storeName = storeName;
+	}
+	public String getStoreCode() {
+		return storeCode;
+	}
+	public void setStoreCode(String storeCode) {
+		this.storeCode = storeCode;
+	}
+	public Date getLoginDate() {
+		return loginDate;
+	}
+	public void setLoginDate(Date loginDate) {
+		this.loginDate = loginDate;
+	}
+	
 }
